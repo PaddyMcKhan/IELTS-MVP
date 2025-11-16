a88009de3f1bcc7d7744393dc72dc263e35a92b8
PaddyMcKhan <anime.canvas.vietnam@gmail.com>
2025-11-16 05:03:55 -0800

    feat: add AI scoring with OpenAI

---
+++ .next/server/app/api/score/route.js
@@ -0,0 +1,7 @@
+var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/score/route.js")
+R.c("server/chunks/[root-of-the-server]__03fe02e0._.js")
+R.c("server/chunks/[root-of-the-server]__b2da729a._.js")
+R.c("server/chunks/node_modules_next_dist_esm_build_templates_app-route_7fe72404.js")
+R.c("server/chunks/_next-internal_server_app_api_score_route_actions_8319739b.js")
+R.m(53778)
+module.exports=R.m(53778).exports
